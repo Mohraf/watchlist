9bdfd1cf79eed985e42416c751800230b7acb692
mohraf <amosngok@gmail.com>
2022-01-24 12:26:38 +0300

    define request functions

--- app/config.py
+++ app/config.py
@@ -1,11 +1,13 @@
 from distutils.debug import DEBUG
 
+from instance.config import MOVIE_API_KEY
+
 
 class Config:
   '''
   General configuration parent class
   '''
-  pass
+  MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
 
 
 class ProdConfig(Config):
